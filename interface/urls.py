c447c756992e20e88a771508f9587de162b4609d
chandra-frontend-dev <chandrasmrft@gmail.com>
2026-05-27 13:48:23 +0530

    Bidirectional

--- interface/urls.py
+++ interface/urls.py
@@ -5,5 +5,5 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
 
        path('device-data/', views.create_device_data, name='create-device-data'),#Devicedata(machineintreface)
-       path('get-testcode-by-barcode/', views.get_testdetails_by_barcode, name='get_testdetails_by_barcode'),#Bidirectional
+       path('get-testcode-by-barcode/', views.get_testcode_by_barcode, name='get_testcode_by_barcode'),#Bidirectional
 ]
